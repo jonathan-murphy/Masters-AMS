e8f9588c9b509aba9b217d26223b83e3e25c99f8
jonathan-murphy <jrumrphy7@gmail.com>
2016-06-18 10:53:40 +0100

    Grip test finds largest orange object and displays bounding rectangle aroung it

--- ProjectApp/app/src/main/java/com/example/jonny/projectapp/OpenCvActivity.java
+++ ProjectApp/app/src/main/java/com/example/jonny/projectapp/OpenCvActivity.java
@@ -16,17 +16,21 @@ import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.MatOfRect;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Vector;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -36,8 +40,9 @@ public class OpenCvActivity extends AppCompatActivity implements CameraBridgeVie
     private CameraBridgeViewBase mOpenCvCameraView;
     private static final String TAG = "CameraViewFragment";
     CameraBridgeViewBase.CvCameraViewFrame outputFrame;
+    //Vector<Rect> boundRect;
     //MatOfPoint contours;
-    List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+    //List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -74,64 +79,47 @@ public class OpenCvActivity extends AppCompatActivity implements CameraBridgeVie
     }
 
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-//        Imgproc.cvtColor(src, dst, Imgproc.COLOR_RGB2HSV, 3); //3 is HSV Channel
-//        Imgproc.cvtColor(dst, src, Imgproc.COLOR_HSV2RGBA, 4); // 4 is RGBA Channel
-        //CameraBridgeViewBase.CvCameraViewFrame outputFrame;
 
+        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
         Mat inputImage = inputFrame.rgba();
         org.opencv.core.Size blurAmount = new Size(15,15);
         int contourIdx = 0;
         Mat outputImage = new Mat();
         Mat blurredImage = new Mat();
         Mat rangeImage = new Mat();
+        Mat cannyOut = new Mat();
+        Mat drawing =  new Mat(inputImage.size(), CvType.CV_8UC3);
         Scalar lower = new Scalar(0,100,100);
         Scalar upper = new Scalar(30,255,255);
         Scalar color = new Scalar(255, 255, 0);
         Imgproc.cvtColor(inputImage, outputImage , Imgproc.COLOR_RGB2HSV_FULL);
         Imgproc.GaussianBlur(outputImage, blurredImage, blurAmount, 2);
         Core.inRange(blurredImage ,lower , upper, rangeImage);
-        Imgproc.Canny(rangeImage, rangeImage, 10, 100);
-        Imgproc.findContours(rangeImage, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
-        //Imgproc.drawContours(rangeImage, contours, contourIdx, color, -1);
+        Imgproc.Canny(rangeImage, cannyOut, 10, 100);
+        Imgproc.findContours(cannyOut, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        double maxArea = -1;
+        int maxAreaId = -1;
+
+        // finding max contour
+        for (int i = 0; i < contours.size(); i++) {
+            Mat contour = contours.get(i);
+            double contArea = Imgproc.contourArea(contour);
+            if (contArea > maxArea) {
+                maxArea = contArea;
+                maxAreaId = i;
+            }
+        }
 
         MatOfPoint2f approxCurve = new MatOfPoint2f();
-
-//        //For each contour found
-//        for (int i=0; i<contours.size(); i++)
-//        {
-//            //Convert contours(i) from MatOfPoint to MatOfPoint2f
-//            MatOfPoint2f contour2f = new MatOfPoint2f( contours.get(i).toArray() );
-//            //Processing on mMOP2f1 which is in type MatOfPoint2f
-//            double approxDistance = Imgproc.arcLength(contour2f, true)*0.02;
-//            Imgproc.approxPolyDP(contour2f, approxCurve, approxDistance, true);
-//
-//            //Convert back to MatOfPoint
-//            MatOfPoint points = new MatOfPoint( approxCurve.toArray() );
-//
-//            // Get bounding rect of contour
-//            Rect rect = Imgproc.boundingRect(points);
-//
-//            // draw enclosing rectangle (all same color, but you could use variable i to make them unique)
-//            Imgproc.rectangle(rangeImage, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height),color);
-//        }
-
-            //Convert contours(i) from MatOfPoint to MatOfPoint2f
-            MatOfPoint2f contour2f = new MatOfPoint2f( contours.get(0).toArray() );
-            //Processing on mMOP2f1 which is in type MatOfPoint2f
-            double approxDistance = Imgproc.arcLength(contour2f, true)*0.02;
-            Imgproc.approxPolyDP(contour2f, approxCurve, approxDistance, true);
-            //Convert back to MatOfPoint
-            MatOfPoint points = new MatOfPoint( approxCurve.toArray() );
-            // Get bounding rect of contour
-            Rect rect = Imgproc.boundingRect(points);
-            // draw enclosing rectangle (all same color, but you could use variable i to make them unique)
-            Imgproc.rectangle(rangeImage, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height),color);
-
-
-        Imgproc.cvtColor(outputImage, inputImage, Imgproc.COLOR_RGB2RGBA);
-
-        //return inputFrame.rgba();
-        return rangeImage;
+        MatOfPoint2f contour2f = new MatOfPoint2f( contours.get(maxAreaId).toArray() );
+        double approxDistance = Imgproc.arcLength(contour2f, true)*0.02;
+        Imgproc.approxPolyDP(contour2f, approxCurve, approxDistance, true);
+        MatOfPoint points = new MatOfPoint( approxCurve.toArray() ); // converting back to mat point
+        Rect rect = Imgproc.boundingRect(points); // getting bounding rectangle
+        Imgproc.rectangle(inputImage, rect.br(), rect.tl(), color);
+        //Imgproc.drawContours(inputImage, contours, maxAreaIdx, color);
+        return inputImage;
     }
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
