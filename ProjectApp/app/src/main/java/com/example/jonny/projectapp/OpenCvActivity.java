5bab8402211fc2dcfde750365a04a59a4d83b650
jonathan-murphy <jrumrphy7@gmail.com>
2016-06-18 14:04:17 +0100

    Fixed issue where app crashed when there was no orange object in view

--- ProjectApp/app/src/main/java/com/example/jonny/projectapp/OpenCvActivity.java
+++ ProjectApp/app/src/main/java/com/example/jonny/projectapp/OpenCvActivity.java
@@ -40,6 +40,27 @@ public class OpenCvActivity extends AppCompatActivity implements CameraBridgeVie
     private CameraBridgeViewBase mOpenCvCameraView;
     private static final String TAG = "CameraViewFragment";
     CameraBridgeViewBase.CvCameraViewFrame outputFrame;
+
+    double threshold1 = 10;
+    double threshold2 = 100;
+    double prevArea = 0;
+    double calibVal = 0;
+    double maxVal = 0;
+    double increase = 0;
+    int frameNo = 1;
+    long time = 0;
+    long startTime = 0;
+    long runTime = 0;
+    String text;
+    double fontScale = 2;
+    int thickness = 2;
+    Point position = new Point(100, 100);
+
+    org.opencv.core.Size blurAmount = new Size(5,5);
+    Scalar lowerHue = new Scalar(0,100,100);
+    Scalar upperHue = new Scalar(30,255,255);
+    Scalar color = new Scalar(255, 255, 0);
+
     //Vector<Rect> boundRect;
     //MatOfPoint contours;
     //List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
@@ -53,7 +74,7 @@ public class OpenCvActivity extends AppCompatActivity implements CameraBridgeVie
         mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.OpenCvView);
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
-
+        startTime = System.currentTimeMillis();
     }
 
 
@@ -80,24 +101,23 @@ public class OpenCvActivity extends AppCompatActivity implements CameraBridgeVie
 
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
 
+        runTime = System.currentTimeMillis() - startTime;
+
         List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
         Mat inputImage = inputFrame.rgba();
-        org.opencv.core.Size blurAmount = new Size(5,5);
-        Mat outputImage = new Mat();
+        Mat hsvImage = new Mat();
         Mat blurredImage = new Mat();
         Mat rangeImage = new Mat();
         Mat cannyOut = new Mat();
         Mat erodedImage = new Mat();
         Mat dilatedImage = new Mat();
-        Scalar lower = new Scalar(0,100,100);
-        Scalar upper = new Scalar(30,255,255);
-        Scalar color = new Scalar(255, 255, 0);
-        Imgproc.cvtColor(inputImage, outputImage , Imgproc.COLOR_RGB2HSV_FULL);
-        Imgproc.GaussianBlur(outputImage, blurredImage, blurAmount, 2);
-        Core.inRange(blurredImage ,lower , upper, rangeImage);
+
+        Imgproc.cvtColor(inputImage, hsvImage , Imgproc.COLOR_RGB2HSV_FULL);
+        Imgproc.GaussianBlur(hsvImage, blurredImage, blurAmount, 2);
+        Core.inRange(blurredImage ,lowerHue , upperHue, rangeImage);
         Imgproc.dilate(rangeImage , dilatedImage, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(9,9)));
         Imgproc.erode(dilatedImage , erodedImage, Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(9,9)));
-        Imgproc.Canny(erodedImage, cannyOut, 10, 100);
+        Imgproc.Canny(erodedImage, cannyOut, threshold1, threshold2);
         Imgproc.findContours(cannyOut, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
 
         double maxArea = -1;
@@ -113,24 +133,36 @@ public class OpenCvActivity extends AppCompatActivity implements CameraBridgeVie
             }
         }
 
-        String text = String.valueOf(maxArea);
-        double fontScale = 2;
-        int thickness = 2;
-        Point position = new Point(100, 500);
+        text = "STRETCH";
 
-        // center the text
-        //Point textOrg((inputImage.cols - textSize.width)/2, (img.rows + textSize.height)/2);
-        //Point textOrg = new Point(150, 2000);
-        MatOfPoint2f approxCurve = new MatOfPoint2f();
-        MatOfPoint2f contour2f = new MatOfPoint2f( contours.get(maxAreaId).toArray() );
-        double approxDistance = Imgproc.arcLength(contour2f, true)*0.02;
-        Imgproc.approxPolyDP(contour2f, approxCurve, approxDistance, true);
-        MatOfPoint points = new MatOfPoint( approxCurve.toArray() ); // converting back to mat point
-        Rect rect = Imgproc.boundingRect(points); // getting bounding rectangle
-        Imgproc.rectangle(inputImage, rect.br(), rect.tl(), color);
-        Imgproc.putText(inputImage, text, position, 3, 1, new Scalar(255, 255, 255, 255), 2);
-        //Imgproc.putText()
-        Imgproc.drawContours(inputImage, contours, maxAreaId, color, -1);
+        if (runTime> 0 && runTime <=2500){
+            calibVal = maxArea;
+            text = "CALIBRATING";
+        }
+
+        else if (runTime> 3500 && runTime <=4500){
+            maxVal = maxArea;
+        }
+
+        text = String.valueOf(contours.size());
+
+        if (contours.size() > 0){
+            MatOfPoint2f approxCurve = new MatOfPoint2f();
+            MatOfPoint2f contour2f = new MatOfPoint2f(contours.get(maxAreaId).toArray());
+            double approxDistance = Imgproc.arcLength(contour2f, true) * 0.02;
+            Imgproc.approxPolyDP(contour2f, approxCurve, approxDistance, true);
+            MatOfPoint points = new MatOfPoint(approxCurve.toArray()); // converting back to mat point
+            Rect rect = Imgproc.boundingRect(points); // getting bounding rectangle
+            Imgproc.rectangle(inputImage, rect.br(), rect.tl(), color);
+            Imgproc.putText(inputImage, text, position, 3, 1, new Scalar(255, 255, 255, 255), 2);
+            Imgproc.drawContours(inputImage, contours, maxAreaId, color, -1);
+        }
+        else {
+            Imgproc.putText(inputImage, "No band found", position, 3, 1, new Scalar(255, 255, 255, 255), 2);
+        }
+
+        frameNo++;
+        prevArea = maxArea;
         return inputImage;
     }
 
