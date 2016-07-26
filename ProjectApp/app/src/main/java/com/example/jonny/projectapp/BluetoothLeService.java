c8ec3cca0556d69737e2dd1b60f8bb7d14f9be80
jonathan-murphy <jrumrphy7@gmail.com>
2016-07-26 10:12:01 +0100

    Added jump data filter and CT:FT calculations, implemented actigraphy asleep/awake classifications

--- ProjectApp/app/src/main/java/com/example/jonny/projectapp/BluetoothLeService.java
+++ ProjectApp/app/src/main/java/com/example/jonny/projectapp/BluetoothLeService.java
@@ -32,15 +32,17 @@ import android.os.Binder;
 import android.os.Environment;
 import android.os.IBinder;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.opencsv.CSVWriter;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
@@ -50,10 +52,31 @@ import java.util.UUID;
  */
 public class BluetoothLeService extends Service {
 
+    int i = 1;
+    int j = 0;
     int counter = 0;
+    int epochSize = 30;
+    float[] epoch = new float[epochSize];
+    float activity = 0f;
+    float x, y, z, X, Y, Z, xPrev, yPrev, zPrev;
     private final static String TAG = BluetoothLeService.class.getSimpleName();
 
-    ArrayList<String[]> actigraphyData = new ArrayList<String[]>();
+    int sleepLatency = 10; // In minutes
+    int solEpochs = sleepLatency * 60 / epochSize;
+    float activityThreshold = 1;
+    int awakeStateEpochs = 2; // number of high activity epochs required to change state to awake
+    int asleep = 0;
+    int awake = 0;
+    Date bedTime;
+    Date wakeTime;
+    boolean activityFlag = false;
+    boolean sleepFlag = false;
+    float solTime = 0;
+
+    ArrayList<String[]> dataToSave = new ArrayList<String[]>();
+    ArrayList<Float> actigraphyData = new ArrayList<Float>();
+    //Double[] actigraphyData;
+    int[] sleepState;
 
     private BluetoothLeService mBluetoothLeService;
 
@@ -97,6 +120,9 @@ public class BluetoothLeService extends Service {
                 Log.i(TAG, "Attempting to start service discovery:" +
                         mBluetoothGatt.discoverServices());
 
+                bedTime = new Date();
+                Log.i(TAG, String.valueOf(bedTime));
+
             } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
                 intentAction = ACTION_GATT_DISCONNECTED;
                 mConnectionState = STATE_DISCONNECTED;
@@ -112,12 +138,52 @@ public class BluetoothLeService extends Service {
                 try {
                     file.createNewFile();
                     CSVWriter writer = new CSVWriter(new FileWriter(file));
-                    writer.writeAll(actigraphyData);
+                    writer.writeAll(dataToSave);
                     writer.close();
                 } catch (IOException e) {
                     System.out.println(e.getMessage());
                 }
+
+                //DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                wakeTime = new Date();
+                Log.i(TAG, String.valueOf(wakeTime));
+
+                analyseSleep();
                 broadcastUpdate(intentAction);
+            }
+        }
+
+        public void analyseSleep() {
+            for (i = 0; i < actigraphyData.size(); i++) {
+                if (actigraphyData.get(i) > activityThreshold) {
+                    sleepState[i] = 1;
+                }
+
+                if (i > solEpochs && sleepFlag == false) {
+                    for (j = i; j > i - solTime; j--) {
+                        if (sleepState[j] == 1) {
+                            activityFlag = true;
+                            break;
+                        }
+                    }
+                    if (activityFlag == true) {
+                        activityFlag = false;
+                    }
+                    else {
+                        sleepFlag = true;
+                        solTime = i - solEpochs;
+                    }
+                }
+
+                // Determing asleep/awake amounts
+                if (sleepFlag == true) {
+                    if (actigraphyData.get(i) > activityThreshold) {
+                        awake = awake + 1;
+                    }
+                    else {
+                        asleep = asleep + 1;
+                    }
+                }
             }
         }
 
@@ -156,11 +222,6 @@ public class BluetoothLeService extends Service {
                                  final BluetoothGattCharacteristic characteristic) {
         final Intent intent = new Intent(action);
 
-        // This is special handling for the Heart Rate Measurement profile.  Data parsing is
-        // carried out as per profile specifications:
-        // http://developer.bluetooth.org/gatt/characteristics/Pages/CharacteristicViewer.aspx?u=org.bluetooth.characteristic.heart_rate_measurement.xml
-            // For all other profiles, writes the data formatted in HEX.
-
         if (counter == 0) {
             final byte[] data = characteristic.getValue();
 
@@ -178,31 +239,39 @@ public class BluetoothLeService extends Service {
 
 //        2g = 16,384 counts/g    4g = 8,192 counts/g      8g = 4,096 counts/g      16g =  2,048 counts/g
 
-            float x = (xAccel[0] << 8 | (xAccel[1] & 0xff));
-            float y = (yAccel[0] << 8 | (yAccel[1] & 0xff));
-            float z = (zAccel[0] << 8 | (zAccel[1] & 0xff));
+            x = (xAccel[0] << 8 | (xAccel[1] & 0xff));
+            y = (yAccel[0] << 8 | (yAccel[1] & 0xff));
+            z = (zAccel[0] << 8 | (zAccel[1] & 0xff));
 
-            float X = x / 4096;
-            float Y = y / 4096;
-            float Z = z / 4096;
+            X = x / 4096;
+            Y = y / 4096;
+            Z = z / 4096;
 
-//            int time = timeStamp[0] << 24 | (timeStamp[1] & 0xff) << 16 | (timeStamp[2] & 0xff) << 8 | (timeStamp[3] & 0xff);
+            X = X - xPrev;
+            Y = Y - yPrev;
+            Z = Z - zPrev;
+//            String xVal = String.valueOf(X);
+//            String yVal = String.valueOf(Y);
+//            String zVal = String.valueOf(Z);
+            activity = Math.abs(X) + Math.abs(Y) + Math.abs(Z) + activity;
 
-            String xVal = String.valueOf(X);
-            String yVal = String.valueOf(Y);
-            String zVal = String.valueOf(Z);
+            xPrev = X;
+            yPrev = Y;
+            zPrev = Z;
 
-//            Log.i(TAG, "  X: " + xVal + "  Y: " + yVal + "  Z: " + zVal);
+            //actigraphyData .add(new String[] {xVal,yVal,zVal});
 
-            actigraphyData .add(new String[] {xVal,yVal,zVal});
+            if (i == epochSize) {
+                dataToSave .add(new String[] {String.valueOf(activity)});
+                actigraphyData .add(activity);
+                activity = 0f;
+                i = 0;
+            }
 
+            i++;
             sendBroadcast(intent);
             counter = 99;
         }
-//        String size = String.valueOf(actigraphyData.size());
-
-//        Log.i("SIZE", size);
-//        Log.i("COUNTER", String.valueOf(counter));
         counter--;
     }
 
@@ -290,7 +359,6 @@ public class BluetoothLeService extends Service {
         // parameter to false.
         mBluetoothGatt = device.connectGatt(this, true, mGattCallback);
         Log.d(TAG, "Trying to create a new connection.");
-        Toast.makeText(getBaseContext(), "CONNECTED", Toast.LENGTH_SHORT);
         mBluetoothDeviceAddress = address;
         mConnectionState = STATE_CONNECTING;
 
--- ProjectApp/app/src/main/java/com/example/jonny/projectapp/JumpFragment.java
+++ ProjectApp/app/src/main/java/com/example/jonny/projectapp/JumpFragment.java
@@ -18,6 +18,7 @@ package com.example.jonny.projectapp;
         import android.view.View;
         import android.view.ViewGroup;
         import android.widget.Button;
+        import android.widget.TextView;
         import android.widget.Toast;
 
         import com.amazonaws.auth.CognitoCachingCredentialsProvider;
@@ -35,8 +36,10 @@ package com.example.jonny.projectapp;
         import java.io.File;
         import java.io.FileWriter;
         import java.io.IOException;
+        import java.lang.reflect.Array;
         import java.text.SimpleDateFormat;
         import java.util.ArrayList;
+        import java.util.Arrays;
         import java.util.Date;
         import java.util.Timer;
         import java.util.TimerTask;
@@ -60,23 +63,60 @@ public class JumpFragment extends Fragment implements SensorEventListener{
     float gMax = -100;
     long maxTime = 0;
     long minTime = 0;
+    Double calibrationVal = 0.0;
     float difference;
     boolean getStart = true;
-    long startTime;
+    int startTime;
     float ecThreshold = 0.3f;
     float prevGForce = 0f;
     float gForce, gForceRaw = 0f;
     float ALPHA = 0.15f;
     float rawX, filterX, rawY, filterY, rawZ, filterZ;
-    int sampleRate = 50; // Accelerometer read speed
+    static int sampleRate = 50; // Accelerometer read speed
     boolean save = false;
-    static int samples = 0;
-    static int recordTime = 3; // enter time to record in seconds
+    static int recordTime = 4; // enter time to record in seconds
+    static int samples = recordTime * sampleRate; // calculating neccesary number of samples to record
     static int beepDelay = 3000; // enter time in milliseconds
-    ArrayList<String[]> mylist = new ArrayList<String[]>();
+    ArrayList<String[]> dataFile = new ArrayList<String[]>();
+    Float[] mylist = new Float[samples];
+    Double[] filtered = new Double[samples];
     long time = 0;
     int i = 0;
     int timerSync = 0;
+
+    double eccentricThreshold = 10.3;
+    double peakThreshold = 0.1;
+    double peakRolloff = 0.5;
+    int startSamples = 30;
+    int concentricTime = 1000;
+    int landedTime = 0;
+    boolean jumpStarted = false;
+    boolean flightStarted = false;
+    boolean landed = false;
+    boolean touchdown = false;
+    int touchdownTime = 0;
+    int landedBound = 0;
+
+    TextView cTime;
+    TextView fTime;
+
+    int contractionTime;
+    int flightTime;
+
+    // filter coefficients //
+    float a1 = 1f;
+    float a2 = -1.7293f;
+    float a3 = 1.4461f;
+    float a4 = -0.5713f;
+    float a5 = 0.0917f;
+    float b1 = 0.0148f;
+    float b2 = 0.0593f;
+    float b3 = 0.089f;
+    float b4 = 0.0593f;
+    float b5 = 0.0148f;
+
+    double in1, in2, in3, in4, in5;
+    double filt1, filt2, filt3, filt4, filt5;
 
     boolean testComplete = false;
 
@@ -101,6 +141,8 @@ public class JumpFragment extends Fragment implements SensorEventListener{
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Arrays.fill(filtered, 0.0);
+
         transferUtility = awsSetup.getTransferUtility(getActivity());
     }
 
@@ -113,8 +155,6 @@ public class JumpFragment extends Fragment implements SensorEventListener{
         //((AppCompatActivity) getActivity()).getSupportActionBar().show();
 
         v = (Vibrator) getActivity().getSystemService(Context.VIBRATOR_SERVICE);
-
-        samples = recordTime * sampleRate; // calculating neccesary number of samples to record
 
         mToneGenerator = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
         mSensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
@@ -170,31 +210,34 @@ public class JumpFragment extends Fragment implements SensorEventListener{
         rawZ = event.values[2];
         time = System.currentTimeMillis();
 
-        filterX = filterX + ALPHA * (rawX - filterX);
-        filterY = filterY + ALPHA * (rawY - filterY);
-        filterZ = filterZ + ALPHA * (rawZ - filterZ);
-
-        gForce = Math.abs(filterX)+Math.abs(filterY)+Math.abs(filterZ);
-        gForceRaw = Math.abs(rawX)+Math.abs(rawY)+Math.abs(rawZ);
+//        filterX = filterX + ALPHA * (rawX - filterX);
+//        filterY = filterY + ALPHA * (rawY - filterY);
+//        filterZ = filterZ + ALPHA * (rawZ - filterZ);
+//
+//        gForce = Math.abs(filterX)+Math.abs(filterY)+Math.abs(filterZ);
+        gForce = Math.abs(rawX)+Math.abs(rawY)+Math.abs(rawZ);
 
         String alphaVal = "Alpha = " + ALPHA;
 
         if (i == 0) {
             Date curDate = new Date();
-            SimpleDateFormat format = new SimpleDateFormat("dd-MM-yy hh:mm:ss a");
-            DateToStr = format.format(curDate);
-            mylist.add(new String[]{DateToStr});
-            mylist.add(new String[]{"Time","X","Y","Z"});
+//            SimpleDateFormat format = new SimpleDateFormat("dd-MM-yy hh:mm:ss a");
+//            DateToStr = format.format(curDate);
+//            mylist.add(new String[]{DateToStr});
+//            mylist.add(new String[]{"Time","X","Y","Z"});
             i = i+1;
         }
 
-        if (i < samples + 1 && save == true) {
-            mylist.add(new String[] {Long.toString(time),Float.toString(rawX),Float.toString(rawY),Float.toString(rawZ)});
+        if (i < samples && save == true) {
+            dataFile.add(new String[] {Long.toString(time),Float.toString(rawX),Float.toString(rawY),Float.toString(rawZ),Float.toString(gForce)});
+            mylist[i] = gForce;
             i = i+1;
         }
 
         if (i == samples && testComplete == false) {
             v.vibrate(250);
+            filter();
+            analyseJump();
             Snackbar snack = Snackbar.make(getActivity().findViewById(android.R.id.content), "Test complete", Snackbar.LENGTH_INDEFINITE)
                     .setAction("SAVE", new View.OnClickListener() {
                         @Override
@@ -205,6 +248,92 @@ public class JumpFragment extends Fragment implements SensorEventListener{
             snack.show();
             testComplete = true;
         }
+    }
+
+    public void analyseJump (){
+
+        for (i = 10; i<=startSamples; i++){
+            calibrationVal = filtered[i] + calibrationVal;
+            Log.i("calibration", String.valueOf(calibrationVal));
+        }
+
+        eccentricThreshold = (calibrationVal / (startSamples-9))* 1.05;
+        Double eccentricThreshold2 = (calibrationVal / (startSamples-9))* 0.95;
+        Log.i("eccentricThreshold", String.valueOf(eccentricThreshold));
+
+        peakThreshold = eccentricThreshold * 0.01;
+        Log.i("peakThreshold", String.valueOf(peakThreshold));
+        peakThreshold = 0.1;
+
+        for (i = startSamples; i<samples-10; i++)
+        {
+            if (jumpStarted == false) {
+                if (filtered[i] >= eccentricThreshold || filtered[i] <= eccentricThreshold2) {
+                    jumpStarted = true;
+                    startTime = i;
+                    Toast.makeText(getActivity().getApplicationContext(), "start " + i, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            if (filtered[i] > filtered[i+1] + peakThreshold && filtered[i] > filtered[i-1] + peakThreshold && i > concentricTime + 10 && landed == false && filtered[i+10] < filtered[i] * peakRolloff ){
+                landedTime = i;
+                landed = true;
+                Toast.makeText(getActivity().getApplicationContext(), "landed "+i, Toast.LENGTH_SHORT).show();
+            }
+
+            if (filtered[i] > filtered[i+1] + peakThreshold && filtered[i] > filtered[i-1] + peakThreshold && flightStarted == false && filtered[i+10] < filtered[i] * peakRolloff ){
+                flightStarted = true;
+                concentricTime = i;
+                Toast.makeText(getActivity().getApplicationContext(), "flight "+i, Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        contractionTime = (concentricTime - startTime) * (1000/sampleRate);
+        flightTime = (landedTime - concentricTime) * (1000/sampleRate);
+
+        for (i = landedTime; i > landedTime-20; i--)
+        {
+            if (touchdown == false ){
+//                touchdown = true;
+//                touchdownTime = i;
+//                Toast.makeText(getActivity().getApplicationContext(), "touchdown "+i, Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        cTime = (TextView)getActivity().findViewById(R.id.CT);
+        cTime.setText("CT: " + contractionTime + "ms");
+
+        fTime = (TextView)getActivity().findViewById(R.id.FT);
+        fTime.setText("FT: " + flightTime + "ms");
+
+
+        dataFile.add(new String[] {"Start",(Double.toString(startTime))});
+        dataFile.add(new String[] {"Land",(Double.toString(landedTime))});
+        dataFile.add(new String[] {"Contraction",(Double.toString(concentricTime))});
+        dataFile.add(new String[] {"Threshold",(Double.toString(eccentricThreshold))});
+    }
+
+    public void filter() {
+
+        for (i = 1; i < samples; i++) {
+            in5 = in4;
+            in4 = in3;
+            in3 = in2;
+            in2 = in1;
+            in1 = mylist[i];
+
+            filt1 = filtered[i];
+            filt5 = filt4;
+            filt4 = filt3;
+            filt3 = filt2;
+            filt2 = filt1;
+
+            //filt1 = a1*in1  + a2*in2 + a3*in3 + a4*in4 + a5*in5 - b1*filt1  - b2*filt2 - b3*filt3 - b4*filt4 - b5*filt5;
+            filt1 = b1*in1  + b2*in2 + b3*in3 + b4*in4 + b5*in5 - a2*filt1 - a3*filt2 - a4*filt3 - a5*filt4;
+            filtered[i] = filt1;
+            dataFile.add(new String[] {(Double.toString(filtered[i]))});
+        }
+
     }
 
     @Override
@@ -234,7 +363,7 @@ public class JumpFragment extends Fragment implements SensorEventListener{
         try {
             file.createNewFile();
             CSVWriter writer = new CSVWriter(new FileWriter(file));
-            writer.writeAll(mylist);
+            writer.writeAll(dataFile);
             writer.close();
         } catch (IOException e) {
             System.out.println(e.getMessage());
